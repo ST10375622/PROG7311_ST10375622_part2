8f82e0606eab2d477fcf43107d0978b7949b7e30
ST10375622 <128041756+ST10375622@users.noreply.github.com>
2025-05-07 15:47:39 +0200

    Add project files.

---
+++ PROG7311_ST10375622_part2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG7311_ST10375622_part2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
